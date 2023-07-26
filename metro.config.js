5e3241c662d61e0fb4c65ca5afa43671f244149a
Juvelineorb4 <73950715+Juvelineorb4@users.noreply.github.com>
2023-07-26 15:10:48 -0500

    metro-config update

--- metro.config.js
+++ metro.config.js
@@ -6,10 +6,11 @@ const { sourceExts } = resolver;
 config.transformer.babelTransformerPath = require.resolve(
   "react-native-css-transformer"
 );
-
+config.transformer.minifierConfig.compress.drop_console = true;
 config.resolver = {
-  sourceExts: [...sourceExts, "css"],
+  sourceExts: [...sourceExts, "css", "mjs", "cjs"],
   blacklistRE: [/#current-cloud-backend\/.*/],
 };
 
 module.exports = config;
+
