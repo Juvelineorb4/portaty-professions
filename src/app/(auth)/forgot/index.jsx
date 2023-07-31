67c6c236f30f22914cc299c65dfdbc8ff929a224
Juvelineorb4 <73950715+Juvelineorb4@users.noreply.github.com>
2023-07-31 17:07:48 -0500

    auth completed maybe

--- src/app/(auth)/forgot/index.jsx
+++ src/app/(auth)/forgot/index.jsx
@@ -16,18 +16,41 @@ import styles from "@/assets/styles/Forgot.module.css";
 import { useForm } from "react-hook-form";
 import { es } from "@/assets/constants/lenguage";
 import { useRouter } from "expo-router";
+import { Auth } from "aws-amplify";
 
-const Forgot = ({ navigation }) => {
+const Forgot = () => {
   const router = useRouter();
   const global = require("@/assets/styles/global.js");
-  const { control, watch } = useForm({
+  const { control, watch, handleSubmit } = useForm({
     defaultValues: {
-      phone: "",
-      code: ["", "", "", "", "", ""],
+      email: "",
     },
   });
-  const phone = watch("phone");
+
+  const email = watch("email");
+  const pwd = watch("password");
   const EMAIL_REGEX = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+  const onHandleSendCodeEmail = async (data) => {
+    const { email, password } = data;
+    try {
+      await Auth.forgotPassword(email);
+      const passwordReplace = password.toString().replace(/%/g, "~~pct~~");
+      const codificado = encodeURIComponent(passwordReplace);
+      router.push({
+        pathname: `/forgot/confirm`,
+        params: {
+          email: email,
+          password: codificado,
+        },
+      });
+    } catch (error) {
+      const response = new Error(error);
+      const { message } = response;
+      console.log("ERROR AL ENVIAR CODIGO", message);
+    }
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -43,8 +66,8 @@ const Forgot = ({ navigation }) => {
             <Text style={styles.title}>{es.authentication.forgot.title}</Text>
             <CustomInput
               control={control}
-              name={`phone`}
-              placeholder={`+58 123 4567`}
+              name={`email`}
+              placeholder={es.authentication.forgot.email.placeholder}
               styled={{
                 text: styles.textInput,
                 label: [styles.labelInput, global.topGray],
@@ -52,21 +75,62 @@ const Forgot = ({ navigation }) => {
                 placeholder: styles.placeholder,
                 input: [styles.inputContainer, global.bgWhiteSoft],
               }}
-              icon={require(`../../../assets/images/phone.png`)}
-              text={`Número de teléfono`}
+              icon={require("@/assets/images/email.png")}
+              text={es.authentication.forgot.email.title}
               rules={{
-                required: "Ingresa tu número de teléfono",
+                required: "requiere correo electronico",
               }}
             />
             <Text style={[styles.code, global.topGray]}>
               {es.authentication.forgot.code}{" "}
-              <Text style={styles.emailText}>{phone}</Text>
+              <Text style={styles.emailText}>{email}</Text>
             </Text>
+            <CustomInput
+              control={control}
+              name={`password`}
+              placeholder={es.authentication.forgot.password.placeholder}
+              styled={{
+                text: styles.textInput,
+                label: styles.labelInput,
+                error: styles.errorInput,
+                placeholder: styles.placeholder,
+                input: [styles.inputContainer, global.bgWhiteSoft],
+              }}
+              text={es.authentication.forgot.password.title}
+              icon={require("@/assets/images/password.png")}
+              security={true}
+              rules={{
+                required: es.authentication.forgot.password.rules,
+                minLength: {
+                  value: 8,
+                  message: "Mínimo 8 caracteres",
+                },
+              }}
+            />
+            <CustomInput
+              control={control}
+              name={`password-repeat`}
+              placeholder={es.authentication.forgot.repeat.placeholder}
+              styled={{
+                text: styles.textInput,
+                label: styles.labelInput,
+                error: styles.errorInput,
+                placeholder: styles.placeholder,
+                input: [styles.inputContainer, global.bgWhiteSoft],
+              }}
+              text={es.authentication.forgot.repeat.title}
+              icon={require("@/assets/images/password.png")}
+              security={true}
+              rules={{
+                required: es.authentication.forgot.repeat.rules,
+                validate: (value) => value == pwd || "No coincide",
+              }}
+            />
           </ScrollView>
           <View style={{ height: 60 }}>
             <CustomButton
               text={es.authentication.forgot.button}
-              handlePress={() => router.replace("/(auth)/forgot/confirm")}
+              handlePress={handleSubmit(onHandleSendCodeEmail)}
               textStyles={[styles.textContinue, global.white]}
               buttonStyles={[styles.continue, global.mainBgColor]}
             />
--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,7 +1,11 @@
 import { Slot, Stack } from "expo-router";
 import { useFonts } from "expo-font";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
+// amplify
+import { Amplify, Hub } from "aws-amplify";
+import awsExports from "@/aws-exports";
+Amplify.configure(awsExports);
 
 SplashScreen.preventAutoHideAsync();
 const Navigation = () => {
@@ -20,6 +24,29 @@ const Navigation = () => {
     boldItalic: require("../assets/fonts/Montserrat-BoldItalic.ttf"),
     name: require("../assets/fonts/ConeriaScript.ttf"),
   });
+  console.log("HOLA")
+
+  useEffect(() => {
+    // crear subscripcion
+    const unsubscribe = Hub.listen("auth", ({ payload: { event, data } }) => {
+      console.log("HUB: ", event);
+      switch (event) {
+        case "signIn":
+          break;
+        case "signOut":
+          break;
+        case "confirmSignUp":
+          break;
+        case "autoSignIn":
+          break;
+        case "updateUserAttributes":
+          break;
+      }
+    });
+    // Preguntar si el usuario existe
+
+    return unsubscribe;
+  }, []);
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
