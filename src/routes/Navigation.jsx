c94e0ddb698eff1a6e169026615ebea9939693a6
chrisesbueno <alvarezchristopherve@gmail.com>
2023-10-06 14:41:46 -0400

    No se que estoy haciendo

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -8,7 +8,7 @@ import Tabs from "./Tabs/Tabs";
 import NavSettings from "./NavSettings";
 // recoil
 import { useRecoilValue, useRecoilState } from "recoil";
-import { userAuthenticated, mapUser, checkRenderState } from "@/atoms";
+import { userAuthenticated, mapUser } from "@/atoms";
 // linking
 import SharePage from "@/screens/Search/SharePage";
 import linking from "./linking";
@@ -23,52 +23,75 @@ import Loading from "@/components/Loading";
 const Navigation = () => {
   const Stack = createNativeStackNavigator();
   const global = require("@/utils/styles/global.js");
-
+  const [checkRender, setCheckRender] = useState(true);
   const [userState, setUserState] = useState(false);
   const userAuth = useRecoilValue(userAuthenticated);
-  const checkRender = useRecoilValue(checkRenderState);
   // pido localizacion
   const { location } = useLocation();
+  const renderNavigation = () => {
+    try {
+      if (userAuth !== null) setUserState(true);
+      setCheckRender(false);
+    } catch (error) {
+      console.log(error);
+      setCheckRender(false);
+    }
+  };
+
+  useEffect(() => {
+    renderNavigation();
+  }, [userAuth]);
 
   return (
-    <NavigationContainer  fallback={<SplashScreen />}>
+    <NavigationContainer linking={linking} fallback={<Loading />}>
       <NavSettings />
-      <Stack.Navigator>
-        {userAuth && checkRender ? (
+      {checkRender === false ? (
+        <Stack.Navigator>
+          {userState ? (
+            <Stack.Screen
+              name={`Tabs_Navigation`}
+              component={Tabs}
+              options={{
+                headerShown: false,
+              }}
+            />
+          ) : (
+            <Stack.Screen
+              name={`Login_Welcome`}
+              component={LoginNavigator}
+              options={{
+                headerShown: false,
+              }}
+            />
+          )}
           <Stack.Screen
-            name={`Tabs_Navigation`}
-            component={Tabs}
+            name="SharePage"
+            component={SharePage}
+            options={{
+              animation: "slide_from_right",
+              header: (props) => <LeftHeaderShare {...props} />,
+            }}
+          />
+          <Stack.Screen
+            name="ShareNavigator"
+            component={ShareNavigator}
+            options={{
+              animation: "slide_from_right",
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Loading"
+            component={<Loading />}
             options={{
               headerShown: false,
             }}
           />
-        ) : (
-          <Stack.Screen
-            name={`Login_Welcome`}
-            component={LoginNavigator}
-            options={{
-              headerShown: false,
-            }}
-          />
-        )}
-
-        <Stack.Screen
-          name="SharePage"
-          component={SharePage}
-          options={{
-            animation: "slide_from_right",
-            header: (props) => <LeftHeaderShare {...props} />,
-          }}
-        />
-        <Stack.Screen
-          name="ShareNavigator"
-          component={ShareNavigator}
-          options={{
-            animation: "slide_from_right",
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
