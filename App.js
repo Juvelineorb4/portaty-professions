f0322c45a261be9c39d8934aeff62a835e8f1843
Juvelineorb4 <73950715+Juvelineorb4@users.noreply.github.com>
2024-07-08 01:44:12 -0400

    add unions chris v2

--- App.js
+++ App.js
@@ -17,16 +17,16 @@ import * as SplashScreen from "expo-splash-screen";
 import { api } from "@/utils/constants/api.jsx";
 import * as WebBrowser from 'expo-web-browser';
 const ENDPOINT =
-  Constants?.AppOwnership?.Expo === "expo"
+  Constants?.AppOwnership?.Expo === ""
     ? api?.stage_endpoint?.dev
     : api?.stage_endpoint?.prod;
 
 const REDIRECT_SIGNIN =
-  Constants?.AppOwnership?.Expo === "expo"
+  Constants?.AppOwnership?.Expo === ""
     ? api?.rediret_signin?.dev
     : api?.rediret_signin?.prod;
 const REDIRECT_SIGNOUT =
-  Constants?.AppOwnership?.Expo === "expo"
+  Constants?.AppOwnership?.Expo === ""
     ? api?.rediret_signout?.dev
     : api?.rediret_signout?.prod;
 console.log("ENDPOINT: ", ENDPOINT);
